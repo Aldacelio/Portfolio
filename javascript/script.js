af891e01c777aa05c733414744a3dc1887873a44
Antonio Aldacélio Cavalcante <82114921+Aldacelio@users.noreply.github.com>
2022-10-13 06:02:16 -0300

    Portfolio resposividades da sessão home e conhecimentos

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,11 @@
 var btnMenu = document.querySelector(".btn-menu");
-
+var menuClick = document.querySelector(".menu");
 btnMenu.addEventListener("click", function(){
+    document.querySelector(".side-bar").classList.toggle("show-menu");
+    document.querySelector(".container").classList.toggle("show-menu");
+});
+
+menuClick.addEventListener("click", function(){
     document.querySelector(".side-bar").classList.toggle("show-menu");
     document.querySelector(".container").classList.toggle("show-menu");
 });
